bcf3cea7a6eeb98c59d7556ab10ef0f3e30b94d2
Stetsed <stetsed@protonmail.com>
2023-04-21 20:52:30 +0200

    Added Package

--- src/user.rs
+++ src/user.rs
@@ -1,7 +1,5 @@
 use std::io::{self, Write};
-use std::env;
 use std::process::Command;
-use std::fs;
 use std::thread;
 use std::time::Duration;
 
@@ -28,14 +26,14 @@ pub fn user() {
     if input.trim().eq_ignore_ascii_case("y") {
         user_extras_stetsed();
     }
-    
+
     // Print a thank-you message and exit the program
     print!("Thank you for using Stetsed's Installer! Hope it helped you! :");
     std::process::exit(0);
 }
 
 // This function creates the home directory for the current user by executing a series of shell commands.
-pub fn user_create_home() -> std::io::Result<String>  {
+pub fn user_create_home() -> std::io::Result<String> {
     // Get the current user's name
     let output = Command::new("whoami")
         .output()
@@ -45,7 +43,10 @@ pub fn user_create_home() -> std::io::Result<String>  {
     // Create the user's home directory and set the owner and permissions
     let commands = vec![
         format!("sudo mkdir /home/{}", whoami_output),
-        format!("sudo chown {}:{} -R /home/{}", whoami_output, whoami_output, whoami_output),
+        format!(
+            "sudo chown {}:{} -R /home/{}",
+            whoami_output, whoami_output, whoami_output
+        ),
         format!("sudo chmod 700 /home/{}", whoami_output),
     ];
 
@@ -58,7 +59,7 @@ pub fn user_create_home() -> std::io::Result<String>  {
     Ok(format!("Home Created"))
 }
 
-pub fn user_yay_packages() -> std::io::Result<String>  {
+pub fn user_yay_packages() -> std::io::Result<String> {
     // Get the username of the current user
     let output = Command::new("whoami")
         .output()
@@ -72,7 +73,7 @@ pub fn user_yay_packages() -> std::io::Result<String>  {
         // Build and install the yay package manager
         format!("cd /home/{} && cd yay-bin && makepkg -s && sudo pacman -U --noconfirm yay-bin* && cd .. && rm -rf yay-bin", whoami_output),
         // Install packages using yay
-        "yay -Syu --noconfirm --answerdiff=None imagemagick kitty ripgrep unzip bat pavucontrol pipewire-pulse dunst bluedevil bluez-utils brightnessctl grimblast-git neovim network-manager-applet rofi-lbonn-wayland-git starship thunar thunar-archive-plugin thunar-volman  webcord-bin wl-clipboard librewolf-bin neofetch swaybg waybar-hyprland-git btop tldr swaylock-effects obsidian fish hyprland npm xdg-desktop-portal-hyprland-git exa noto-fonts-emoji qt5-wayland qt6-wayland blueman swappy playerctl wlogout sddm-git nano ttf-jetbrains-mono-nerd lazygit swayidle".to_string(),
+        "yay -Syu --noconfirm --answerdiff=None imagemagick rust-analyzer kitty ripgrep unzip bat pavucontrol pipewire-pulse dunst bluedevil bluez-utils brightnessctl grimblast-git neovim network-manager-applet rofi-lbonn-wayland-git starship thunar thunar-archive-plugin thunar-volman  webcord-bin wl-clipboard librewolf-bin neofetch swaybg waybar-hyprland-git btop tldr swaylock-effects obsidian fish hyprland npm xdg-desktop-portal-hyprland-git exa noto-fonts-emoji qt5-wayland qt6-wayland blueman swappy playerctl wlogout sddm-git nano ttf-jetbrains-mono-nerd lazygit swayidle".to_string(),
     ];
 
     for command in commands {
@@ -83,30 +84,29 @@ pub fn user_yay_packages() -> std::io::Result<String>  {
 
 // Function to install dotfiles from a user's repository or Stetsed's Dotfiles
 pub fn user_install_dotfiles() -> std::io::Result<String> {
-
     // Initialize variables to hold the URLs of the dotfiles repositories
-    let mut dotfiles_url = String::new(); 
-    let mut ssh_url = String::new(); 
+    let mut dotfiles_url = String::new();
+    let mut ssh_url = String::new();
 
     // Ask the user if they want to use Stetsed's Dotfiles or their own
-    let mut input = String::new(); 
-    print!("Do you want to use Stetsed's Dotfiles? (y/n): "); 
-    io::stdout().flush().unwrap(); 
-    io::stdin().read_line(&mut input).unwrap(); 
+    let mut input = String::new();
+    print!("Do you want to use Stetsed's Dotfiles? (y/n): ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
 
     // If the user wants to use Stetsed's Dotfiles, set the URLs accordingly
     if input.trim().eq_ignore_ascii_case("y") {
-        dotfiles_url = "https://github.com/Stetsed/.dotfiles.git".to_owned(); 
-        ssh_url = "git@github.com:Stetsed/.dotfiles.git".to_owned(); 
+        dotfiles_url = "https://github.com/Stetsed/.dotfiles.git".to_owned();
+        ssh_url = "git@github.com:Stetsed/.dotfiles.git".to_owned();
     } else {
         // Ask the user for their github dotfiles repository and set the URLs accordingly
-        print!("Enter your github dotfiles repository (username/repository_name): "); 
-        let mut dotfiles_repo = String::new(); 
-        io::stdout().flush().unwrap(); 
-        io::stdin().read_line(&mut dotfiles_repo).unwrap(); 
-        let dotfiles_repo_trim = dotfiles_repo.trim().to_owned(); 
-        dotfiles_url = format!("https://github.com/{}.git", dotfiles_repo_trim); 
-        ssh_url = format!("git@github.com:{}.git", dotfiles_repo_trim); 
+        print!("Enter your github dotfiles repository (username/repository_name): ");
+        let mut dotfiles_repo = String::new();
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut dotfiles_repo).unwrap();
+        let dotfiles_repo_trim = dotfiles_repo.trim().to_owned();
+        dotfiles_url = format!("https://github.com/{}.git", dotfiles_repo_trim);
+        ssh_url = format!("git@github.com:{}.git", dotfiles_repo_trim);
     }
 
     // Create a vector of commands to execute
@@ -127,12 +127,11 @@ pub fn user_install_dotfiles() -> std::io::Result<String> {
 
 // Function to enable and configure various system services
 pub fn user_extras() -> std::io::Result<String> {
-
     // Create a vector of commands to execute
     let commands = vec![
         "sudo systemctl enable --now bluetooth && sudo systemctl enable sddm", // Enable and start Bluetooth and SDDM services
         "systemctl --user enable --now pipewire", // Enable and start the user-level pipewire service
-        "systemctl enable --now pipewire-pulse", // Enable and start the pipewire-pulse service
+        "systemctl enable --now pipewire-pulse",  // Enable and start the pipewire-pulse service
         "sudo timedatectl set-ntp true && sudo timedatectl set-timezone Europe/Amsterdam", // Set the timezone and enable NTP synchronization
     ];
 
@@ -146,7 +145,6 @@ pub fn user_extras() -> std::io::Result<String> {
 
 // Function to enable and configure various system services specific to Stetsed's setup
 pub fn user_extras_stetsed() -> std::io::Result<String> {
-
     // Create a vector of commands to execute
     let commands = vec![
         "echo '10.4.78.251:/mnt/Vault/Storage /mnt/data nfs defaults,_netdev,x-systemd.automount,x-systemd.mount-timeout=10,noauto 0 0' | sudo tee -a /etc/fstab", // Add an NFS mount to /etc/fstab
@@ -173,7 +171,10 @@ pub fn execute_command(command: &str) -> std::io::Result<()> {
         .expect("Failed to execute command");
 
     if !output.status.success() {
-        eprintln!("Command '{}' failed with exit status: {:?}", command, output.status);
+        eprintln!(
+            "Command '{}' failed with exit status: {:?}",
+            command, output.status
+        );
         std::process::exit(1);
     }
 
@@ -183,4 +184,3 @@ pub fn execute_command(command: &str) -> std::io::Result<()> {
 
     Ok(())
 }
-
